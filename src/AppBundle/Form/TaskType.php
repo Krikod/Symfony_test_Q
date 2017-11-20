577428ac53426e84f5113b03ce7b1b05ad4d3783
Krikod <kristen.kod@gmail.com>
2017-11-20 19:26:48 +0100

    Form class

---
+++ src/AppBundle/Form/TaskType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('task')
+            ->add('dueDate', null, array('widget' => 'single_text'))
+            ->add('save', SubmitType::class)
+            ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_bundle_task';
+    }
+}
