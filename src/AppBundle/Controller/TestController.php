e14bf2f92a161be8bd50a47ab2aa7ff7fe058627
Krikod <kristen.kod@gmail.com>
2017-11-20 22:49:49 +0100

    form -> controller

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -2,7 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Form\TaskType;
+use Symfony\Component\BrowserKit\Request;
 
 class TestController extends Controller
 {
@@ -10,4 +13,27 @@ class TestController extends Controller
     {
         return $this->render('@App/index.html.twig');
     }
+
+    public function newAction(Request $request)
+    {
+        $task = new Task();
+
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+
+            return $this->redirectToRoute('task_success');
+        }
+        return $this->render('@App/index.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
 }
--- src/AppBundle/Form/TaskType.php
+++ src/AppBundle/Form/TaskType.php
@@ -2,7 +2,10 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\Task;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,14 +16,21 @@ class TaskType extends AbstractType
     {
         $builder
             ->add('task')
-            ->add('dueDate', null, array('widget' => 'single_text'))
+            ->add('dueDate', DateType::class, array(
+                'widget' => 'single_text',
+                'label' => 'Due Date',
+                'required' => false
+                ))
+//            ->add('agreeTerms', CheckboxType::class, array('mapped' => false))
             ->add('save', SubmitType::class)
             ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-
+        $resolver->setDefaults(array(
+            'data_class' => Task::class,
+        ));
     }
 
     public function getBlockPrefix()
